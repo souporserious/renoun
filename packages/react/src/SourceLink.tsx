27d0956ddae30f41fff45a58829a20395679d344
Travis Arnold <ftntravis@gmail.com>
2022-12-30 23:14:19 -0800

    add SourceLink component

---
+++ packages/react/src/SourceLink.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { DataProviderContext } from './DataProvider'
+
+export function SourceLink({
+  children = 'View Source',
+  className,
+}: {
+  children?: React.ReactNode
+  className?: string
+}) {
+  const { dataItem } = React.useContext(DataProviderContext)
+
+  return (
+    <a
+      href={dataItem.path}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={className}
+    >
+      {children}
+    </a>
+  )
+}
--- site/app/[slug]/page.tsx
+++ site/app/[slug]/page.tsx
@@ -1,18 +1,22 @@
 import allDocs from 'mdxts/docs'
-import { Content, DataProvider, Outline, SiblingNavigation } from '@mdxts/react'
+import {
+  Content,
+  DataProvider,
+  Outline,
+  SiblingNavigation,
+  SourceLink,
+} from '@mdxts/react'
 
 export async function generateStaticParams() {
   return allDocs.map((doc) => ({ slug: doc.slug }))
 }
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const doc = allDocs.find((doc) => doc.slug === params.slug)
-
   return (
     <DataProvider data={allDocs} slug={params.slug}>
       <div style={{ display: 'flex' }}>
         <div>
-          <a href={doc.path}>Source</a>
+          <SourceLink />
           <Content />
           <SiblingNavigation />
         </div>
