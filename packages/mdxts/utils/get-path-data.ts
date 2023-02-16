caaf98a4527a7ed3bd52c47e4620596e7f44f804
Travis Arnold <ftntravis@gmail.com>
2023-02-15 23:05:09 -0800

    add docs for getPathData

--- packages/mdxts/utils/get-path-data.ts
+++ packages/mdxts/utils/get-path-data.ts
@@ -1,9 +1,13 @@
-/** Returns the active and sibling data based on the active slug. */
-export function getPathData(allData: any[], activeSlug: string) {
+/** Returns the active and sibling data based on the active pathname. */
+export function getPathData(
+  /** The collected data from the specified mdxts loaders. */
+  allData: any[],
+
+  /** The pathname of the active page. */
+  pathname: string
+) {
   const data = Object.values(allData) as any[]
-  const activePathIndex = data.findIndex(
-    (data) => data.slug === activeSlug.replace('/', '')
-  )
+  const index = data.findIndex((data) => data.pathname === pathname)
 
   function getSiblingPath(startIndex: number, direction: number) {
     const siblingIndex = startIndex + direction
@@ -17,8 +21,8 @@ export function getPathData(allData: any[], activeSlug: string) {
   }
 
   return {
-    active: data[activePathIndex] || null,
-    previous: getSiblingPath(activePathIndex, -1),
-    next: getSiblingPath(activePathIndex, 1),
+    active: data[index] || null,
+    previous: getSiblingPath(index, -1),
+    next: getSiblingPath(index, 1),
   }
 }
