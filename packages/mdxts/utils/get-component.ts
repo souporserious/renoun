ef8c425dbb3d5d7837c252ded5d8a3cea82316db
Travis Arnold <ftntravis@gmail.com>
2023-02-15 23:03:33 -0800

    add getComponent utility

---
+++ packages/mdxts/utils/get-component.ts
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import * as jsxRuntime from 'react/jsx-runtime'
+import * as jsxDevRuntime from 'react/jsx-dev-runtime'
+
+/**
+ * Execute a string of code and return the default export.
+ * Supports TypeScript and JSX syntax.
+ *
+ * @example
+ *
+ * import { Content } from 'mdxts/components'
+ * import allDocs from 'mdxts/docs'
+ *
+ * export default function Page({ params }: { params: { slug: string } }) {
+ *   const doc = allDocs.find((doc) => doc.slug === params.slug)
+ *   const Component = await getComponent(doc.mdx.code)
+ *   return <Component />
+ * }
+ */
+export async function getComponent<ComponentType extends any>(
+  /** The code to execute. */
+  code: string,
+
+  /** An object of external dependencies that will be available to the code. */
+  dependencies: Record<string, any> = {}
+) {
+  const allDependencies = {
+    react: React,
+    'react/jsx-runtime': jsxRuntime,
+    'react/jsx-dev-runtime': jsxDevRuntime,
+    ...dependencies,
+  }
+  const functionExports = { default: null }
+  const functionModule: {
+    exports: { default: React.ComponentType<ComponentType> | null }
+  } = { exports: functionExports }
+  const functionRequire = (path) => {
+    if (allDependencies[path]) {
+      return allDependencies[path]
+    }
+    throw Error(`Module not found: ${path}.`)
+  }
+  const result = new Function('module', 'exports', 'require', code)
+
+  result(functionModule, functionExports, functionRequire)
+
+  return functionModule.exports.default
+}
--- packages/mdxts/utils/index.ts
+++ packages/mdxts/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './get-data'
+export * from './get-component'
 export * from './get-editor-path'
 export * from './get-examples'
 export * from './get-path-data'
