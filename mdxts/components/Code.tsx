1625fc0169ef9c1af263ac60664f57ad2fd13825
Travis Arnold <ftntravis@gmail.com>
2023-08-29 09:13:02 -0700

    always throw

--- mdxts/components/Code.tsx
+++ mdxts/components/Code.tsx
@@ -8,32 +8,29 @@ type CodeProps = {
 }
 
 async function AsyncCode({ language = 'bash', value, ...props }: CodeProps) {
-  try {
-    const { lines } = await highlight(
-      value.trim(),
-      language,
-      JSON.parse(await readFile(process.env.MDXTS_THEME_PATH, 'utf-8'))
-    )
+  throw new Error(
+    `CWD: ${process.cwd()}, attempting to load: ${process.env.MDXTS_THEME_PATH}`
+  )
 
-    return (
-      <pre style={{ gridArea: '1 / 1', margin: 0 }} {...props}>
-        {lines.map((line, index) => (
-          <div key={index} style={{ height: 20, lineHeight: '20px' }}>
-            {line.map((token, index) => (
-              <span key={index} style={token.style}>
-                {token.content}
-              </span>
-            ))}
-          </div>
-        ))}
-      </pre>
-    )
-  } catch (error) {
-    throw new Error(
-      `${process.cwd()}, attempting to load: ${process.env.MDXTS_THEME_PATH}`,
-      { cause: error }
-    )
-  }
+  const { lines } = await highlight(
+    value.trim(),
+    language,
+    JSON.parse(await readFile(process.env.MDXTS_THEME_PATH, 'utf-8'))
+  )
+
+  return (
+    <pre style={{ gridArea: '1 / 1', margin: 0 }} {...props}>
+      {lines.map((line, index) => (
+        <div key={index} style={{ height: 20, lineHeight: '20px' }}>
+          {line.map((token, index) => (
+            <span key={index} style={token.style}>
+              {token.content}
+            </span>
+          ))}
+        </div>
+      ))}
+    </pre>
+  )
 }
 
 /** Renders a code block with syntax highlighting. */
