a76f1bc467596c777b3f485158095fc1a05d46f8
Travis Arnold <ftntravis@gmail.com>
2023-08-29 08:54:21 -0700

    try catch

--- mdxts/components/Code.tsx
+++ mdxts/components/Code.tsx
@@ -8,30 +8,32 @@ type CodeProps = {
 }
 
 async function AsyncCode({ language = 'bash', value, ...props }: CodeProps) {
-  console.log(
-    process.cwd(),
-    'attempting to load:',
-    process.env.MDXTS_THEME_PATH
-  )
-  const { lines } = await highlight(
-    value.trim(),
-    language,
-    JSON.parse(await readFile(process.env.MDXTS_THEME_PATH, 'utf-8'))
-  )
+  try {
+    const { lines } = await highlight(
+      value.trim(),
+      language,
+      JSON.parse(await readFile(process.env.MDXTS_THEME_PATH, 'utf-8'))
+    )
 
-  return (
-    <pre style={{ gridArea: '1 / 1', margin: 0 }} {...props}>
-      {lines.map((line, index) => (
-        <div key={index} style={{ height: 20, lineHeight: '20px' }}>
-          {line.map((token, index) => (
-            <span key={index} style={token.style}>
-              {token.content}
-            </span>
-          ))}
-        </div>
-      ))}
-    </pre>
-  )
+    return (
+      <pre style={{ gridArea: '1 / 1', margin: 0 }} {...props}>
+        {lines.map((line, index) => (
+          <div key={index} style={{ height: 20, lineHeight: '20px' }}>
+            {line.map((token, index) => (
+              <span key={index} style={token.style}>
+                {token.content}
+              </span>
+            ))}
+          </div>
+        ))}
+      </pre>
+    )
+  } catch (error) {
+    throw new Error(
+      `${process.cwd()}, attempting to load: ${process.env.MDXTS_THEME_PATH}`,
+      { cause: error }
+    )
+  }
 }
 
 /** Renders a code block with syntax highlighting. */
