91f197bf562d37d3a843ceb1f6dbf0f3d45631fa
Travis Arnold <ftntravis@gmail.com>
2023-08-28 22:17:05 -0700

    try moving theme to public directory

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,7 +1,7 @@
 import { createMDXTSPlugin } from 'mdxts/next'
 
 const withMDXTS = createMDXTSPlugin({
-  theme: 'theme.json',
+  theme: 'public/mdxts/theme.json',
   gitSource: 'https://github.com/souporserious/mdxts/tree/main',
 })
 
