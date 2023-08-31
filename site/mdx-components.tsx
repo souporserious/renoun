3065f961db9c8b55661ba53a49af66b2411b203a
Travis Arnold <ftntravis@gmail.com>
2023-08-30 22:02:06 -0700

    stub out components

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -5,9 +5,14 @@ import { getLanguageFromClassName } from 'mdxts/utils'
 
 import theme from './theme.json'
 
-export function useMDXComponents(): MDXComponents {
+export function useMDXComponents() {
   return {
     Example: (props) => <div {...props} />,
+    Editor: (props) => <div {...props} />,
+    Preview: (props) => <div {...props} />,
+    Error: (props) => <div {...props} />,
+    Outline: (props) => <div {...props} />,
+    References: (props) => <div {...props} />,
     Summary: (props) => <div {...props} />,
     pre: (props) => {
       const { children, className } = (props.children as any).props
