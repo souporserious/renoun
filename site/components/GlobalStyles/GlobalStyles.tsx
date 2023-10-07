3b66c47c10b3a19dc3bc8eb14883e0c4d87c8731
Travis Arnold <ftntravis@gmail.com>
2023-10-07 16:36:07 -0700

    update createGlobalStyle to tagged template literal

--- site/components/GlobalStyles/GlobalStyles.tsx
+++ site/components/GlobalStyles/GlobalStyles.tsx
@@ -1,31 +1,35 @@
 import { createGlobalStyle } from 'styled-components'
 import { cssVariables, mediaQuery } from 'theme'
 
-export const GlobalStyles = createGlobalStyle({
-  ':root': cssVariables ,
-  '*': {
-    boxSizing: 'border-box',
-  },
-  html: {
-    fontSize: '40%',
-    [String(mediaQuery.medium)]: {
-      fontSize: '56%',
-    },
-    [String(mediaQuery.large)]: {
-      fontSize: '72%',
-    },
-    [String(mediaQuery.xlarge)]: {
-      fontSize: '100%',
-    },
-  },
-  body: {
-    margin: 0,
-    fontFamily: "'Inter', sans-serif",
-    background: '#2d0e46',
-    color: 'white',
-  },
-  '#root': {
-    width: '100%',
-    minHeight: '100vh',
-  },
-})
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    ${Object.entries(cssVariables)
+      .map(([key, value]) => `${key}: ${value};`)
+      .join('\n')}
+  }
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 40%;
+    ${mediaQuery.medium} {
+      font-size: 56%;
+    }
+    ${mediaQuery.large} {
+      font-size: 72%;
+    }
+    ${mediaQuery.xlarge} {
+      font-size: 100%;
+    }
+  }
+  body {
+    margin: 0;
+    font-family: 'Inter', sans-serif;
+    background: #2d0e46;
+    color: white;
+  }
+  #root {
+    width: 100%;
+    min-height: 100vh;
+  }
+`
