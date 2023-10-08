e506976cfa83bfb9ba3b45f98d917d9d4443822f
Travis Arnold <ftntravis@gmail.com>
2023-10-08 16:39:20 -0700

    only render editor for live code blocks

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import type { MDXComponents } from 'mdx/types'
 import { getMetadataFromClassName } from 'mdxts/utils'
+import { Code } from 'mdxts/components/server'
 import { Editor } from './editor'
-
 import theme from './theme.json'
 
 export function useMDXComponents() {
@@ -15,29 +15,29 @@ export function useMDXComponents() {
     References: (props) => <div {...props} />,
     Summary: (props) => <div {...props} />,
     Note: (props) => <div {...props} />,
-    pre: (props) => {
-      const {
-        children: value,
-        live,
-        className = '',
-      } = (props.children as any).props
-      const metadata = getMetadataFromClassName(className)
+    pre: ({
+      live,
+      children,
+    }: {
+      live?: boolean
+      children: React.ReactElement
+    }) => {
+      const value = children.props.children.trim()
+      const metadata = getMetadataFromClassName(children.props.className || '')
 
-      // return live ? (
-      return (
+      return live ? (
         <Editor
           defaultValue={value.trim()}
           language={metadata?.language}
           theme={theme as any}
         />
+      ) : (
+        <Code
+          language={metadata?.language}
+          value={value.trim()}
+          theme={theme as any}
+        />
       )
-      // ) : (
-      //   <Code
-      //     language={metadata?.language}
-      //     value={value.trim()}
-      //     theme={theme}
-      //   />
-      // )
     },
   } satisfies MDXComponents
 }
