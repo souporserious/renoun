e4a9235b20e0be5fd547c3072a9ecfc8f622258f
Travis Arnold <ftntravis@gmail.com>
2023-10-26 22:20:17 -0700

    add warning

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -14,6 +14,19 @@ export default function RootLayout({
         <link rel="canonical" href="https://www.mdxts.com" />
       </head>
       <body>
+        <div
+          style={{
+            fontWeight: 600,
+            fontSize: 14,
+            padding: '1rem',
+            backgroundColor: '#d39e5a',
+            color: '#1c1309',
+            textAlign: 'center',
+          }}
+        >
+          This package is still a work in progress. The APIs are not stable and
+          may change.
+        </div>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
