884d50cdedc2f35c7cf453c8701bd688660eb99d
Travis Arnold <ftntravis@gmail.com>
2023-11-17 00:40:20 -0800

    don't require context provider

--- mdxts/src/context.tsx
+++ mdxts/src/context.tsx
@@ -2,18 +2,14 @@
 import React, { createContext } from 'react'
 import { type CodeBlocks } from './remark'
 
-const MdxtsContext = createContext<{ codeBlocks: CodeBlocks } | null>(null)
+const MdxtsContext = createContext<{ codeBlocks: CodeBlocks }>({
+  codeBlocks: [],
+})
 
 export function MdxtsProvider({ value, children }) {
   return <MdxtsContext.Provider value={value}>{children}</MdxtsContext.Provider>
 }
 
 export function useMdxtsContext() {
-  const context = React.useContext(MdxtsContext)
-
-  if (context === null) {
-    throw new Error('useMdxtsContext must be used within a MdxtsProvider')
-  }
-
-  return context
+  return React.useContext(MdxtsContext)
 }
