fc464a2e6337e3f731134c7eaa9b088088fee4db
Travis Arnold <ftntravis@gmail.com>
2023-12-08 11:00:06 -0800

    fix git source links

--- mdxts/src/utils/get-source-path.ts
+++ mdxts/src/utils/get-source-path.ts
@@ -1,4 +1,7 @@
+import { findRootSync } from '@manypkg/find-root'
 import { getEditorPath } from './get-editor-path'
+
+let rootDirectory = null
 
 /**
  * Returns a constructed source path for the local IDE in development or a git link in production.
@@ -7,6 +10,8 @@ export function getSourcePath(path: string) {
   if (process.env.NODE_ENV === 'development') {
     return getEditorPath({ path })
   }
-
-  return `${process.env.MDXTS_GIT_SOURCE}${path.replace(process.cwd(), '')}`
+  if (rootDirectory === null) {
+    rootDirectory = findRootSync(process.cwd()).rootDir
+  }
+  return `${process.env.MDXTS_GIT_SOURCE}${path.replace(rootDirectory, '')}`
 }
