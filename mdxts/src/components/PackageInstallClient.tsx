7d2598db4619a2c572e34afd4fb40da0cc6f0500
Travis Arnold <ftntravis@gmail.com>
2023-12-09 13:37:54 -0800

    working on local storage package manager preference

--- mdxts/src/components/PackageInstallClient.tsx
+++ mdxts/src/components/PackageInstallClient.tsx
@@ -1,5 +1,40 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+
+const packageStyles = `
+.PackageInstallButton {
+  background-color: #fff;
+  color: #000;
+}
+.PackageInstallButton.selected {
+  background-color: #000;
+  color: #fff;
+}
+`.trim()
+
+function useLocalStorageState(key: string, defaultValue: string) {
+  const [state, setState] = useState(defaultValue)
+  const initialRender = useRef(true)
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return
+    }
+
+    if (initialRender.current) {
+      const saved = localStorage.getItem(key)
+      if (saved) {
+        setState(saved)
+      }
+    } else {
+      localStorage.setItem(key, state)
+    }
+
+    initialRender.current = false
+  }, [state])
+
+  return [state, setState] as const
+}
 
 /**
  * The client-side component for the PackageInstall component.
@@ -13,32 +48,48 @@ export function PackageInstallClient({
     React.ReactElement<{ value: string; language: string }>
   >
 }) {
-  const [selectedPackageManager, setSelectedPackageManager] = useState('npm')
-  const command = allHighlightedCommands[selectedPackageManager]
+  const stateKey = 'package-manager'
+  const defaultPackageManager = 'npm'
+  const [activePackageManager, setActivePackageManager] = useLocalStorageState(
+    stateKey,
+    defaultPackageManager
+  )
+  const command = allHighlightedCommands[activePackageManager]
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <style>{packageStyles}</style>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         {Object.keys(allHighlightedCommands).map((packageManager) => {
-          const isSelected = selectedPackageManager === packageManager
+          const isSelected = activePackageManager === packageManager
           return (
             <button
               key={packageManager}
-              onClick={() => setSelectedPackageManager(packageManager)}
+              id={packageManager}
+              onClick={() => setActivePackageManager(packageManager)}
+              className={
+                isSelected
+                  ? 'PackageInstallButton selected'
+                  : 'PackageInstallButton'
+              }
               style={{
                 padding: '0.5rem 1rem',
-                backgroundColor: isSelected ? '#000' : '#fff',
-                color: isSelected ? '#fff' : '#000',
                 border: '1px solid #000',
                 borderRadius: '0.5rem',
                 cursor: 'pointer',
                 margin: '0.5rem',
               }}
+              suppressHydrationWarning
             >
               {packageManager}
             </button>
           )
         })}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `localStorage.getItem('${stateKey}') && document.getElementById('${defaultPackageManager}').classList.remove('selected'); document.getElementById(localStorage.getItem('${stateKey}')).classList.add('selected')`,
+          }}
+        />
       </div>
       {command}
     </div>
