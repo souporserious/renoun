a11f02842f1ba51d7710e428d115964bbabcd12f
Travis Arnold <ftntravis@gmail.com>
2023-12-23 21:42:34 -0800

    more docs cleanup

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                     borderRadius: '1rem',
                   }}
                 >
-                  Beta
+                  Preview
                 </span>
               </header>
               <h1
--- site/next.config.mjs
+++ site/next.config.mjs
@@ -3,14 +3,7 @@ import { createMdxtsPlugin } from 'mdxts/next'
 const withMdxts = createMdxtsPlugin({
   theme: 'theme.json',
   gitSource: 'https://github.com/souporserious/mdxts',
-  types: [
-    'react',
-    'mdxts',
-    'mdxts/components',
-    'mdxts/next',
-    'mdxts/rehype',
-    'mdxts/remark',
-  ],
+  types: ['react', 'mdxts', 'mdxts/components', 'mdxts/next'],
 })
 
 export default withMdxts({
