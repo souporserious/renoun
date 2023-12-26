2f0703e0c1244e7882ab6b71db749abb8e02eb90
Travis Arnold <ftntravis@gmail.com>
2023-12-25 22:49:21 -0800

    close mobile menu on route change

--- site/components/Sidebar/NavigationBoundary.tsx
+++ site/components/Sidebar/NavigationBoundary.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { useEffect, useRef } from 'react'
+import { usePathname } from 'next/navigation'
 import { useSignalValue } from 'hooks/useSignal'
 
 import { isNavigationOpenSignal } from './NavigationToggle'
@@ -9,7 +11,17 @@ export function NavigationBoundary({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+  const previousPathname = useRef<string>()
   const isNavigationOpen = useSignalValue(isNavigationOpenSignal)
+
+  useEffect(() => {
+    if (previousPathname.current !== pathname) {
+      isNavigationOpenSignal.value = false
+    }
+    previousPathname.current = pathname
+  }, [pathname])
+
   return (
     <div className={styles.container} data-open={isNavigationOpen}>
       {children}
