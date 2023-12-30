049fc8a1ecd164c3ba2697162a1c9cb2af854693
Travis Arnold <ftntravis@gmail.com>
2023-12-30 13:06:12 -0800

    use relative code example

--- mdxts/src/components/Code.examples.tsx
+++ mdxts/src/components/Code.examples.tsx
@@ -1,32 +1,8 @@
 import React from 'react'
 import { Code } from './Code'
 
-const value = `
-import React, { useState } from 'react';
-
-export function Counter() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <div>
-      <h2>Counter: {count}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  );
-}
-`
-
 export function Basic() {
-  return <Code value={value} language="tsx" />
+  return <Code source="./counter/useCounter.ts" />
 }
 
 export function Inline() {
