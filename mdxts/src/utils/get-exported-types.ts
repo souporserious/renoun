401eb0e7c9a8cb58288eeb0b0e7a15cad4c913ec
Travis Arnold <ftntravis@gmail.com>
2024-01-02 13:44:24 -0800

    use flat map instead of type guard

--- mdxts/src/utils/get-exported-types.ts
+++ mdxts/src/utils/get-exported-types.ts
@@ -2,10 +2,7 @@ import { kebabCase } from 'case-anything'
 import type { SourceFile } from 'ts-morph'
 import { Node } from 'ts-morph'
 import { getSymbolDescription, isJsxComponent } from '@tsxmod/utils'
-import {
-  getFunctionParameterTypes,
-  type PropertyMetadata,
-} from './get-function-parameter-types'
+import { getFunctionParameterTypes } from './get-function-parameter-types'
 
 export type ExportedType = NonNullable<
   ReturnType<typeof getFunctionParameterTypes>
@@ -13,40 +10,30 @@ export type ExportedType = NonNullable<
 
 /** Gets all exported prop types from a source file. */
 export function getExportedTypes(sourceFile: SourceFile) {
-  return Array.from(sourceFile.getExportedDeclarations())
-    .map(([name, [declaration]]) => {
-      if (
-        Node.isFunctionDeclaration(declaration) ||
-        Node.isFunctionExpression(declaration) ||
-        Node.isArrowFunction(declaration)
-      ) {
-        const filePath = declaration.getSourceFile().getFilePath()
-        const types = getFunctionParameterTypes(declaration) || []
-        const symbol = declaration.getSymbol()
+  return (
+    Array.from(sourceFile.getExportedDeclarations())
+      .map(([name, [declaration]]) => {
+        if (
+          Node.isFunctionDeclaration(declaration) ||
+          Node.isFunctionExpression(declaration) ||
+          Node.isArrowFunction(declaration)
+        ) {
+          const filePath = declaration.getSourceFile().getFilePath()
+          const types = getFunctionParameterTypes(declaration) || []
+          const symbol = declaration.getSymbol()
 
-        return {
-          name,
-          types: types as PropertyMetadata[],
-          description: symbol ? getSymbolDescription(symbol) : null,
-          isComponent: isJsxComponent(declaration),
-          slug: kebabCase(name),
-          filePath: filePath as string,
+          return {
+            name,
+            types,
+            description: symbol ? getSymbolDescription(symbol) : null,
+            isComponent: isJsxComponent(declaration),
+            slug: kebabCase(name),
+            filePath: filePath as string,
+          }
         }
-      }
-      return null
-    })
-    .filter(
-      (
-        source
-      ): source is NonNullable<{
-        name: string
-        types: PropertyMetadata[]
-        description: string | null
-        isComponent: boolean
-        slug: string
-        filePath: string
-      }> => {
-        return Boolean(source)
-      }
-    )
+        return null
+      })
+      // Filter out null values and satisfy types.
+      .flatMap((entry) => (entry ? [entry] : []))
+  )
 }
