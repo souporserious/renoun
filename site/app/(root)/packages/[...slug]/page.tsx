17bf665af7cd700b705994eae19090808fa99e1c
Travis Arnold <ftntravis@gmail.com>
2024-01-04 00:11:00 -0800

    fix off-center badge

--- site/app/(root)/packages/[...slug]/page.tsx
+++ site/app/(root)/packages/[...slug]/page.tsx
@@ -54,7 +54,7 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
             gap: '1rem',
           }}
         >
-          {title ? <h1>{title}</h1> : null}
+          {title ? <h1 style={{ margin: 0 }}>{title}</h1> : null}
           {isServerOnly ? (
             <span
               style={{
