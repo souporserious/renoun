cc354525c20d6cb83d7ba47978fcff56cc9ab6bb
Travis Arnold <ftntravis@gmail.com>
2024-01-25 20:35:27 -0800

    add icon to Note component

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -31,11 +31,14 @@ export function useMDXComponents() {
     Outline: (props) => <div {...props} />,
     References: (props) => <div {...props} />,
     Summary: (props) => <div {...props} />,
-    Note: (props) => (
+    Note: ({ children, ...props }) => (
       <div
         style={{
           fontSize: 'var(--font-size-body-2)',
           lineHeight: 'var(--line-height-body-2)',
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr',
+          gap: '0.75rem',
           padding: '1em',
           margin: '1rem 0',
           border: '1px solid var(--color-separator)',
@@ -43,7 +46,23 @@ export function useMDXComponents() {
           backgroundColor: 'var(--color-surface-2)',
         }}
         {...props}
-      />
+      >
+        <svg
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          width="1rem"
+          height="1rem"
+          viewBox="0 0 24 24"
+          style={{ marginTop: '0.25rem', opacity: 0.6 }}
+        >
+          <path d="M7.06883 21.6H16.219C18.7973 21.6 20.8879 19.5093 20.8879 16.9312V5.86885C20.8879 3.29074 18.7973 1.20001 16.219 1.20001H7.06883C4.49072 1.20001 2.39999 3.29074 2.39999 5.86885V16.9312C2.39999 19.5093 4.49072 21.6 7.06883 21.6Z" />
+          <path d="M15.3946 15.842H7.89178M15.3946 11.245H7.89178M10.755 6.6586H7.89232" />
+        </svg>
+        <div>{children}</div>
+      </div>
     ),
     ul: (props) => (
       <ul
