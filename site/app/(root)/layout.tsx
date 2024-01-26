dfe07343d73fb0ce32c09e4fffd2f24970b4b472
Travis Arnold <ftntravis@gmail.com>
2024-01-25 20:38:47 -0800

    remove experimental banner

--- site/app/(root)/layout.tsx
+++ site/app/(root)/layout.tsx
@@ -3,24 +3,9 @@ import styles from './layout.module.css'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div
-        style={{
-          fontWeight: 600,
-          fontSize: 'var(--font-size-body-2)',
-          padding: '1rem',
-          backgroundColor: '#d39e5a',
-          color: '#1c1309',
-          textAlign: 'center',
-        }}
-      >
-        This package is still experimental. The APIs are not stable and may
-        change. Use at your own risk.
-      </div>
-      <div className={styles.container}>
-        <Sidebar />
-        <main className="prose">{children}</main>
-      </div>
-    </>
+    <div className={styles.container}>
+      <Sidebar />
+      <main className="prose">{children}</main>
+    </div>
   )
 }
