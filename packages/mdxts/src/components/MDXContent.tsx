ccbed3a1285cdc15a557ac8e78d72f0a958f327c
Travis Arnold <ftntravis@gmail.com>
2024-02-21 09:42:34 -0800

    add server only for MDXContent

--- packages/mdxts/src/components/MDXContent.tsx
+++ packages/mdxts/src/components/MDXContent.tsx
@@ -3,6 +3,7 @@ import * as jsxRuntime from 'react/jsx-runtime'
 import * as jsxDevRuntime from 'react/jsx-dev-runtime'
 import type { MDXComponents } from 'mdx/types'
 import { compile } from '@mdx-js/mdx'
+import 'server-only'
 
 /** Compiles and renders MDX content. */
 export async function MDXContent({
