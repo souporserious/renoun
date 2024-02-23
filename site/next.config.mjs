b4701d7a29888a6f3858be8b18151936ae7caafa
Travis Arnold <ftntravis@gmail.com>
2024-02-22 22:32:10 -0800

    remove types from config

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -3,7 +3,6 @@ import { createMdxtsPlugin } from 'mdxts/next'
 const withMdxts = createMdxtsPlugin({
   theme: 'theme.json',
   gitSource: 'https://github.com/souporserious/mdxts',
-  types: ['react', 'mdxts', 'mdxts/components', 'mdxts/next'],
 })
 
 export default withMdxts({
