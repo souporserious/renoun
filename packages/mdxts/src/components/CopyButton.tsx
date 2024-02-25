53d8112fce681a2547ba762d83a645fb7ed85db3
Travis Arnold <ftntravis@gmail.com>
2024-02-25 02:26:22 -0800

    revert font size change to CopyButton

--- packages/mdxts/src/components/CopyButton.tsx
+++ packages/mdxts/src/components/CopyButton.tsx
@@ -30,8 +30,8 @@ export function CopyButton({ value }: { value: string }) {
       }}
     >
       <svg
-        width="var(--font-size-body-3)"
-        height="var(--font-size-body-3)"
+        width="1rem"
+        height="1rem"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
