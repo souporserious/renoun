33baf4d6192c8d6bc1b07c778c32291e3a124102
Travis Arnold <ftntravis@gmail.com>
2024-02-25 01:39:26 -0800

    clean up head

--- site/app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
-}
--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="canonical" href="https://www.mdxts.dev" />
         <link
           rel="icon"
