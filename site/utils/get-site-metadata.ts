2404a3c6da6af8e35c16b59021e41eccb26db0fe
Travis Arnold <ftntravis@gmail.com>
2024-04-09 23:00:00 -0700

    pass metadata through

--- site/utils/get-site-metadata.ts
+++ site/utils/get-site-metadata.ts
@@ -6,6 +6,7 @@ export function getSiteMetadata({
   title = 'MDXTS - The Content & Documentation SDK for React',
   description = `Build type-safe content and generate documentation using MDX, TypeScript, and React.`,
   keywords = 'react, mdx, typescript, content, documentation, components, design, systems',
+  ...rest
 }: { title?: string; description?: string } & Omit<
   Metadata,
   'title' | 'description'
@@ -15,6 +16,7 @@ export function getSiteMetadata({
     title,
     description,
     keywords,
+    ...rest,
     openGraph: {
       title: title!,
       description: description!,
@@ -22,10 +24,12 @@ export function getSiteMetadata({
       siteName: 'MDXTS',
       locale: 'en_US',
       type: 'website',
+      ...rest.openGraph,
     },
     twitter: {
       card: 'summary_large_image',
       site: '@souporserious',
+      ...rest.twitter,
     },
   } satisfies Metadata
 }
