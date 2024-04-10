0294b4f94b08a8661d02d1411b79f537014f1e68
Travis Arnold <ftntravis@gmail.com>
2024-04-09 22:33:00 -0700

    fix errors

--- site/app/og/[...slug]/route.tsx
+++ site/app/og/[...slug]/route.tsx
@@ -1,8 +1,6 @@
 import type { NextRequest } from 'next/server'
 import { ImageResponse } from 'next/og'
 
-export const runtime = 'edge'
-
 export const dynamic = 'force-static'
 
 export function generateStaticParams() {
@@ -13,10 +11,6 @@ export async function GET(
   _request: NextRequest,
   { params }: { params: { slug: string[] } }
 ) {
-  const logoSource = await fetch(
-    new URL('../../../public/logo.png', import.meta.url)
-  ).then((response) => response.arrayBuffer())
-
   return new ImageResponse(
     (
       <div
@@ -31,10 +25,6 @@ export async function GET(
           gap: '1rem',
         }}
       >
-        <img
-          src={`data:image/png;base64,${Buffer.from(logoSource).toString('base64')}`}
-          height="100"
-        />
         <span
           style={{
             fontSize: '4rem',
--- site/utils/get-site-metadata.ts
+++ site/utils/get-site-metadata.ts
@@ -6,7 +6,10 @@ export function getSiteMetadata({
   title = 'MDXTS - The Content & Documentation SDK for React',
   description = `Build type-safe content and generate documentation using MDX, TypeScript, and React.`,
   keywords = 'react, mdx, typescript, content, documentation, components, design, systems',
-}: Metadata = {}) {
+}: { title?: string; description?: string } & Omit<
+  Metadata,
+  'title' | 'description'
+> = {}) {
   return {
     metadataBase: new URL(url),
     title,
