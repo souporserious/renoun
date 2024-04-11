99256b5e6461a69df76744348d610cdd1be0643a
Travis Arnold <ftntravis@gmail.com>
2024-04-11 12:28:19 -0700

    fix production base url

--- site/utils/constants.ts
+++ site/utils/constants.ts
@@ -1,4 +1,6 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:4000'
-    : `https://${process.env.VERCEL_URL}`
+  process.env.VERCEL_ENV === 'production'
+    ? process.env.MDXTS_SITE_URL
+    : process.env.NODE_ENV === 'production'
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:4000'
