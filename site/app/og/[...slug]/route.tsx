130f02dc134c7964b56032ff4e34a486378d8fbc
Travis Arnold <ftntravis@gmail.com>
2024-04-11 08:30:40 -0700

    load images once

--- site/app/og/[...slug]/route.tsx
+++ site/app/og/[...slug]/route.tsx
@@ -1,16 +1,19 @@
 import type { NextRequest } from 'next/server'
 import { ImageResponse } from 'next/og'
-import { readFile } from 'node:fs/promises'
+import { readFileSync } from 'node:fs'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { allData } from 'data'
 
-const currentDirectory = dirname(fileURLToPath(import.meta.url))
-
-async function getImageSource(path: string) {
-  const data = await readFile(resolve(currentDirectory, path))
+function getImageSource(path: string) {
+  const currentDirectory = dirname(fileURLToPath(import.meta.url))
+  const data = readFileSync(resolve(currentDirectory, path))
   return `data:image/png;base64,${data.toString('base64')}`
 }
+
+const logoSource = getImageSource('../../../public/logo.png')
+const chevronSource = getImageSource('images/chevron.png')
+const backgroundSource = getImageSource('images/background.png')
 
 export function generateStaticParams() {
   return allData.paths().map((pathname) => ({
@@ -26,14 +29,7 @@ export async function GET(
     ...params.slug.slice(0, -1),
     params.slug.slice(-1).at(0)!.replace('.png', ''),
   ]
-  const [data, logoSource, chevronSource, backgroundSource] = await Promise.all(
-    [
-      allData.get(slug),
-      getImageSource('../../../public/logo.png'),
-      getImageSource('images/chevron.png'),
-      getImageSource('images/background.png'),
-    ]
-  )
+  const data = await allData.get(slug)
 
   if (!data) {
     return new Response('Not found', { status: 404 })
