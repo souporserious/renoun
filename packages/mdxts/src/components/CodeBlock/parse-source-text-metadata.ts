cdeeefe601213ce1b8079d68be9cf7dfc373e7ac
Travis Arnold <ftntravis@gmail.com>
2024-04-25 19:14:33 -0700

    rewrite relative import specifiers to us epackage name

--- packages/mdxts/src/components/CodeBlock/parse-source-text-metadata.ts
+++ packages/mdxts/src/components/CodeBlock/parse-source-text-metadata.ts
@@ -5,6 +5,7 @@ import type { SourceFile } from 'ts-morph'
 import { format, resolveConfig } from 'prettier'
 import 'server-only'
 
+import { getPathRelativeToPackage } from '../../utils/get-relative-path-as-package-import'
 import { isJsxOnly } from '../../utils/is-jsx-only'
 import type { Languages } from './get-tokens'
 import { getLanguage } from './get-tokens'
@@ -112,11 +113,45 @@ export async function parseSourceTextMetadata({
   // Create a ts-morph source file to type-check JavaScript and TypeScript code blocks.
   if (isJavaScriptLikeLanguage) {
     try {
-      sourceFile = project
-        .createSourceFile(filename, finalValue, {
-          overwrite: true,
+      sourceFile = project.createSourceFile(filename, finalValue, {
+        overwrite: true,
+      })
+
+      // Identify and collect missing imports/types to try and resolve theme.
+      // This is specifically the case for examples since they import files relative to the package.
+      // TODO: this needs a ton of optimizing
+      const diagnostics = sourceFile.getPreEmitDiagnostics()
+
+      sourceFile
+        .getImportDeclarations()
+        .filter((importDeclaration) => {
+          return diagnostics.some((diagnostic) => {
+            const diagnosticStart = diagnostic.getStart()
+            if (diagnosticStart === undefined) {
+              return false
+            }
+            return (
+              diagnostic.getCode() === 2307 &&
+              diagnosticStart >= importDeclaration.getStart() &&
+              diagnosticStart <= importDeclaration.getEnd()
+            )
+          })
         })
-        .fixMissingImports()
+        .forEach((importDeclaration) => {
+          importDeclaration.remove()
+        })
+
+      // attempt to fix the removed imports
+      sourceFile.fixMissingImports()
+
+      // remap relative module specifiers to package imports if possible
+      // e.g. `import { getTheme } from '../../mdxts/src/components'` -> `import { getTheme } from 'mdxts/components'`
+      sourceFile.getImportDeclarations().forEach((importDeclaration) => {
+        if (importDeclaration.isModuleSpecifierRelative()) {
+          const importSpecifier = getPathRelativeToPackage(importDeclaration)
+          importDeclaration.setModuleSpecifier(importSpecifier)
+        }
+      })
     } catch (error) {
       if (error instanceof Error) {
         const workingDirectory = process.cwd()
