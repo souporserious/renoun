79086b163425a6b5fa06f544dcdee16d505731af
Travis Arnold <ftntravis@gmail.com>
2024-05-02 18:47:29 -0700

    don't run tests for dist directory

--- packages/mdxts/jest.config.js
+++ packages/mdxts/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
   transformIgnorePatterns: ['!node_modules/'],
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
