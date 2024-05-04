eadd96da2cc189d426db2dad8f5ba14f59636b38
Travis Arnold <ftntravis@gmail.com>
2024-05-04 16:25:14 -0700

    add external icon to content links

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -86,6 +86,39 @@ export function useMDXComponents() {
         <div>{children}</div>
       </div>
     ),
+    a: (props) => {
+      const isExternal = props.href.startsWith('http')
+      return (
+        <a
+          {...props}
+          style={{
+            display: 'inline-flex',
+            alignItems: 'center',
+            gap: '0.25em',
+          }}
+        >
+          {props.children}
+          {isExternal ? (
+            <svg
+              fill="none"
+              stroke="var(--color-foreground-interactive)"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              style={{
+                width: 'max(1rem, 0.8em)',
+                height: 'max(1rem, 0.8em)',
+              }}
+            >
+              <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+              <path d="M15 3h6v6"></path>
+              <path d="M10 14L21 3"></path>
+            </svg>
+          ) : null}
+        </a>
+      )
+    },
     ul: (props) => (
       <ul
         style={{
