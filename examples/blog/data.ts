90a65e55e8e1ce4801b1e524a179c15c6b5e23a0
Travis Arnold <ftntravis@gmail.com>
2024-05-08 01:44:38 -0700

    move example front matter to referenced type

--- examples/blog/data.ts
+++ examples/blog/data.ts
@@ -1,12 +1,14 @@
 import { createSource } from 'mdxts'
 
+type FrontMatter = {
+  title: string
+  date: Date
+  summary?: string
+  tags?: any
+}
+
 export const allPosts = createSource<{
-  frontMatter: {
-    title: string
-    date: Date
-    summary?: string
-    tags?: any
-  }
+  frontMatter: FrontMatter
 }>('posts/*.mdx', {
   baseDirectory: 'posts',
   sort: (a, b) => b.frontMatter.date.getTime() - a.frontMatter.date.getTime(),
