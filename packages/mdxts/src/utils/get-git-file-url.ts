c92ee63406205d176083ac70dfbadfcb9743f891
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-05-27 12:31:22 +0200

    added `gitProvider` prop & unit tests

--- packages/mdxts/src/utils/get-git-file-url.ts
+++ packages/mdxts/src/utils/get-git-file-url.ts
@@ -1,29 +1,35 @@
+import { join } from 'path'
+
 /** Constructs a URL for a specific file in a Git repository. Supports GitHub, GitLab, and Bitbucket. */
 export function getGitFileUrl(
   filePath: string,
   line: number = 0,
   column: number = 0,
   gitSource: string,
-  gitBranch: string = 'main'
+  gitBranch: string = 'main',
+  gitProvider?: string
 ): string {
   const url = new URL(gitSource)
   let fileUrl: string
 
-  switch (url.hostname) {
-    case 'github.com':
-      fileUrl = `${gitSource}/blob/${gitBranch}/${filePath}`
+  const provider =
+    gitProvider || gitSource.replace(/^https?:\/\/(?:.*\.)?(.*?)\..*/gm, `$1`)
+
+  switch (provider) {
+    case 'github':
+      fileUrl = join(url.pathname, '/blob/', gitBranch, filePath)
       if (line || column) fileUrl += `?plain=1`
       if (line) fileUrl += `#L${line}`
       if (column) fileUrl += `:${column}`
       break
 
-    case 'gitlab.com':
-      fileUrl = `${gitSource}/-/blob/${gitBranch}/${filePath}`
+    case 'gitlab':
+      fileUrl = join(url.pathname, '/-/blob/', gitBranch, filePath)
       if (line) fileUrl += `#L${line}`
       break
 
-    case 'bitbucket.org':
-      fileUrl = `${gitSource}/src/${gitBranch}/${filePath}`
+    case 'bitbucket':
+      fileUrl = join(url.pathname, '/src/', gitBranch, filePath)
       if (line) fileUrl += `#lines-${line}`
       break
 
@@ -31,5 +37,5 @@ export function getGitFileUrl(
       throw new Error(`Git provider not recognized for ${gitSource}`)
   }
 
-  return fileUrl
+  return `${url.protocol}//${url.host}${fileUrl}`
 }
