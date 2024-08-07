d738bea548544ee1fda0e962903c83ba55a82cc5
Travis Arnold <ftntravis@gmail.com>
2024-08-07 01:17:04 -0700

    handle pending requests

--- packages/mdxts/src/project/rpc/client.ts
+++ packages/mdxts/src/project/rpc/client.ts
@@ -3,20 +3,39 @@ import type { WebSocketRequest, WebSocketResponse } from './server'
 
 let requestId = 0
 
+type Request = {
+  resolve: (value?: any) => void
+  reject: (reason?: any) => void
+}
+
 export class WebSocketClient {
   #ws: WebSocket
+
   #isConnected = false
-  #requests: {
-    [key: number]: {
-      resolve: (value?: any) => void
-      reject: (reason?: any) => void
-    }
-  } = {}
+
+  #requests: Record<number, Request> = {}
+
+  #pendingRequestIds = new Set<number>()
 
   constructor() {
     this.#ws = new WebSocket(`ws://localhost:${process.env.MDXTS_WS_PORT}`)
-    this.#ws.once('open', () => (this.#isConnected = true))
-    this.#ws.on('message', this.#handleMessage.bind(this))
+
+    this.#ws.addEventListener('open', this.#handleOpen.bind(this))
+
+    this.#ws.addEventListener('message', (event) => {
+      this.#handleMessage(event.data.toString())
+    })
+  }
+
+  #handleOpen() {
+    this.#isConnected = true
+
+    this.#pendingRequestIds.forEach((id) => {
+      this.#ws.send(JSON.stringify(this.#requests[id]))
+    })
+    this.#pendingRequestIds.clear()
+
+    this.#ws.removeEventListener('open', this.#handleOpen)
   }
 
   #handleMessage(message: string) {
@@ -57,14 +76,12 @@ export class WebSocketClient {
           clearTimeout(timeoutId)
           reject(reason)
         },
-      }
+      } satisfies Request
 
       if (this.#isConnected) {
         this.#ws.send(JSON.stringify(request))
       } else {
-        this.#ws.once('open', () => {
-          this.#ws.send(JSON.stringify(request))
-        })
+        this.#pendingRequestIds.add(id)
       }
     })
   }
