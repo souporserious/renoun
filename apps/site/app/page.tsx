7b812a4e16ce6c3fbc587f0a78842e85fa4130c7
Travis Arnold <ftntravis@gmail.com>
2024-08-28 23:10:34 -0700

    better headline

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -2,15 +2,16 @@ export default function Page() {
   return (
     <main css={{ padding: '4rem 0' }}>
       <Logo />
-      <div css={{ maxWidth: '40rem' }}>
-        <h1 css={{ fontSize: 'var(--font-size-heading)' }}>
-          The Toolkit To Build Docs As Great as Your Product
+      <div css={{ maxWidth: '45rem' }}>
+        <h1 css={{ fontSize: 'var(--font-size-heading)', textWrap: 'balance' }}>
+          Docs That Match the Quality of Your Product
         </h1>
         <p
           css={{
             fontSize: 'var(--font-size-body)',
             lineHeight: 'var(--line-height-body)',
             letterSpacing: '0.01em',
+            textWrap: 'balance',
           }}
         >
           Meticulously crafted React components and utilities, designed to
