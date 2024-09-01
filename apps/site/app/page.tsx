4424420ba41cf619afce876597df1e0a4c33dffd
Travis Arnold <ftntravis@gmail.com>
2024-09-01 00:53:12 -0700

    add email signup

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -1,8 +1,17 @@
 import { CodeInline } from 'omnidoc/components'
 
+import { SignupForm } from './SignupForm'
+
 export default function Page() {
   return (
-    <main css={{ padding: '4rem 0' }}>
+    <main
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '4rem 0',
+        gap: '4rem',
+      }}
+    >
       <div css={{ maxWidth: '45rem' }}>
         <h1
           css={{
@@ -35,6 +44,44 @@ export default function Page() {
           paddingY="0.5em"
           css={{ fontSize: 'var(--font-size-code)' }}
         />
+      </div>
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '4rem 4rem 6rem',
+          gap: '3rem',
+          background: '#0d0f10',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '1rem',
+          }}
+        >
+          <h2
+            css={{
+              fontSize: 'var(--font-size-subheading)',
+              margin: 0,
+            }}
+          >
+            Stay Updated
+          </h2>
+          <p
+            css={{
+              fontSize: 'var(--font-size-body)',
+              margin: 0,
+            }}
+          >
+            Get Notified When Omnidoc is ready
+          </p>
+        </div>
+        <SignupForm />
       </div>
     </main>
   )
