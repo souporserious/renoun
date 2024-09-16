05ccd15faa5f6f77d3faf832776c4c36822d7399
Travis Arnold <ftntravis@gmail.com>
2024-09-16 15:26:58 -0700

    remove unnecessary extension alias override

--- examples/design-system/next.config.mjs
+++ examples/design-system/next.config.mjs
@@ -14,10 +14,6 @@ export default withMDX({
   output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   webpack(config) {
-    config.resolve.extensionAlias = {
-      '.js': ['.ts', '.tsx', '.js'],
-    }
-
     config.plugins.push(
       new webpack.ContextReplacementPlugin(
         /\/(@ts-morph\/common)\//,
