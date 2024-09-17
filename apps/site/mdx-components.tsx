39939f92cbabdc1e64f85f4c6e1a6ee4790332cb
Travis Arnold <ftntravis@gmail.com>
2024-09-17 00:28:10 -0700

    add Note component and fix inline code line height

--- apps/site/mdx-components.tsx
+++ apps/site/mdx-components.tsx
@@ -3,9 +3,32 @@ import { CodeBlock, CodeInline } from 'renoun/components'
 
 export function useMDXComponents() {
   return {
+    Note: (props) => {
+      return (
+        <div
+          css={{
+            backgroundColor: '#1b487d',
+            color: 'white',
+            padding: '1em',
+            borderRadius: 5,
+            margin: '1em 0',
+          }}
+          {...props}
+        />
+      )
+    },
     code: (props) => {
       return (
-        <CodeInline value={props.children as string} language="typescript" />
+        <CodeInline
+          value={props.children as string}
+          language="typescript"
+          css={{
+            paddingTop: 0,
+            paddingBottom: 0,
+            lineHeight: 1.2,
+            overflowX: 'auto',
+          }}
+        />
       )
     },
     pre: (props) => {
