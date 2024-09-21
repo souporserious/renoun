e142057eabf3d8ff1ebc0a88c0424c7f20962ac3
Travis Arnold <ftntravis@gmail.com>
2024-09-20 21:40:33 -0700

    move collection docs to dynamic route

---
+++ apps/site/app/(site)/collections/[...slug]/page.tsx
@@ -0,0 +1,31 @@
+import { createCollection, type MDXContent } from 'renoun/collections'
+import type { Headings } from '@renoun/mdx'
+import { notFound } from 'next/navigation'
+
+import { DocumentSource } from '@/components/DocumentSource'
+
+const DocsCollection = createCollection<{
+  default: MDXContent
+  metadata: { title: string; description: string }
+  headings: Headings
+}>('app/[(]site[)]/collections/docs/*.mdx', {
+  baseDirectory: 'app/(site)/collections/docs',
+})
+
+export async function generateStaticParams() {
+  const sources = await DocsCollection.getSources()
+
+  return sources
+    .filter((source) => source.isFile())
+    .map((source) => ({ slug: source.getPathSegments() }))
+}
+
+export default async function Doc({ params }: { params: { slug: string[] } }) {
+  const docSource = DocsCollection.getSource(params.slug)
+
+  if (!docSource) {
+    notFound()
+  }
+
+  return <DocumentSource source={docSource} />
+}
