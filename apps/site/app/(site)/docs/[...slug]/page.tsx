a3002738d13366d0af37903a7294884f5b778703
Travis Arnold <ftntravis@gmail.com>
2024-09-20 20:12:08 -0700

    add DocumentSource component

--- apps/site/app/(site)/docs/[...slug]/page.tsx
+++ apps/site/app/(site)/docs/[...slug]/page.tsx
@@ -1,8 +1,7 @@
 import { notFound } from 'next/navigation'
 
 import { DocsCollection } from '@/collections'
-import { SiblingLink } from '@/components/SiblingLink'
-import { TableOfContents } from '@/components/TableOfContents'
+import { DocumentSource } from '@/components/DocumentSource'
 
 export async function generateStaticParams() {
   const sources = await DocsCollection.getSources()
@@ -19,88 +18,5 @@ export default async function Doc({ params }: { params: { slug: string[] } }) {
     notFound()
   }
 
-  const Content = await docSource.getDefaultExport().getValue()
-  const metadata = await docSource.getNamedExport('metadata').getValue()
-  const headings = await docSource.getNamedExport('headings').getValue()
-  const updatedAt = await docSource.getUpdatedAt()
-  const editPath = docSource.getEditPath()
-  const [previousSource, nextSource] = await docSource.getSiblings({
-    depth: 0,
-  })
-
-  return (
-    <div
-      css={{
-        display: 'grid',
-        gridTemplateColumns: 'minmax(0, 1fr)',
-        gap: '4rem',
-
-        '@media screen and (min-width: 60rem)': {
-          gridTemplateColumns: 'minmax(0, 1fr) 12rem',
-        },
-      }}
-    >
-      <div
-        css={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '4rem',
-        }}
-      >
-        <div className="prose">
-          <h1 css={{ fontSize: '3rem', margin: 0 }}>{metadata.title}</h1>
-          <Content />
-        </div>
-
-        <div css={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}>
-          {updatedAt ? (
-            <div
-              css={{
-                fontSize: 'var(--font-size-body-3)',
-                color: 'var(--color-foreground-secondary)',
-              }}
-            >
-              Last updated{' '}
-              <time
-                dateTime={updatedAt.toString()}
-                itemProp="dateModified"
-                css={{ fontWeight: 600 }}
-              >
-                {updatedAt.toLocaleString('en', {
-                  year: '2-digit',
-                  month: '2-digit',
-                  day: '2-digit',
-                })}
-              </time>
-            </div>
-          ) : null}
-
-          <nav
-            css={{
-              display: 'grid',
-              gridTemplateColumns: '1fr 1fr',
-              gap: '2rem',
-            }}
-          >
-            {previousSource ? (
-              <SiblingLink
-                source={previousSource}
-                direction="previous"
-                variant="title"
-              />
-            ) : null}
-            {nextSource ? (
-              <SiblingLink
-                source={nextSource}
-                direction="next"
-                variant="title"
-              />
-            ) : null}
-          </nav>
-        </div>
-      </div>
-
-      <TableOfContents headings={headings} editPath={editPath} />
-    </div>
-  )
+  return <DocumentSource source={docSource} />
 }
---
+++ apps/site/components/DocumentSource.tsx
@@ -0,0 +1,98 @@
+import type { FileSystemSource, MDXContent } from 'renoun/collections'
+import type { Headings } from '@renoun/mdx'
+
+import { SiblingLink } from './SiblingLink'
+import { TableOfContents } from './TableOfContents'
+
+export async function DocumentSource<
+  Source extends FileSystemSource<{
+    default: MDXContent
+    metadata: { title: string; description: string }
+    headings: Headings
+  }>,
+>({ source }: { source: Source }) {
+  const Content = await source.getDefaultExport().getValue()
+  const metadata = await source.getNamedExport('metadata').getValue()
+  const headings = await source.getNamedExport('headings').getValue()
+  const updatedAt = await source.getUpdatedAt()
+  const editPath = source.getEditPath()
+  const [previousSource, nextSource] = await source.getSiblings({
+    depth: 0,
+  })
+
+  return (
+    <div
+      css={{
+        display: 'grid',
+        gridTemplateColumns: 'minmax(0, 1fr)',
+        gap: '4rem',
+
+        '@media screen and (min-width: 60rem)': {
+          gridTemplateColumns: 'minmax(0, 1fr) 12rem',
+        },
+      }}
+    >
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '4rem',
+        }}
+      >
+        <div className="prose">
+          <h1 css={{ fontSize: '3rem', margin: 0 }}>{metadata.title}</h1>
+          <Content />
+        </div>
+
+        <div css={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}>
+          {updatedAt ? (
+            <div
+              css={{
+                fontSize: 'var(--font-size-body-3)',
+                color: 'var(--color-foreground-secondary)',
+              }}
+            >
+              Last updated{' '}
+              <time
+                dateTime={updatedAt.toString()}
+                itemProp="dateModified"
+                css={{ fontWeight: 600 }}
+              >
+                {updatedAt.toLocaleString('en', {
+                  year: '2-digit',
+                  month: '2-digit',
+                  day: '2-digit',
+                })}
+              </time>
+            </div>
+          ) : null}
+
+          <nav
+            css={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr',
+              gap: '2rem',
+            }}
+          >
+            {previousSource ? (
+              <SiblingLink
+                source={previousSource}
+                direction="previous"
+                variant="title"
+              />
+            ) : null}
+            {nextSource ? (
+              <SiblingLink
+                source={nextSource}
+                direction="next"
+                variant="title"
+              />
+            ) : null}
+          </nav>
+        </div>
+      </div>
+
+      <TableOfContents headings={headings} editPath={editPath} />
+    </div>
+  )
+}
