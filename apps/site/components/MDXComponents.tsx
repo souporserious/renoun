cb60f6846d8cc380c4cf04637938f8ecb59dc1b6
Travis Arnold <ftntravis@gmail.com>
2024-10-10 16:30:20 -0700

    remove note icon for now

--- apps/site/components/MDXComponents.tsx
+++ apps/site/components/MDXComponents.tsx
@@ -84,40 +84,6 @@ export const MDXComponents = {
           },
         }}
       >
-        <div
-          css={{
-            '--translate-y': '5%',
-            display: 'flex',
-            padding: '0.3rem',
-            position: 'absolute',
-            left: 0,
-            top: 'var(--padding-y)',
-            translate: `calc(-50% - var(--border-width) * 0.5) var(--translate-y)`,
-            backgroundColor: '#82AAFF',
-            borderRadius: '100%',
-
-            '@media (min-width: 60rem)': {
-              '--translate-y': '-7%',
-            },
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            css={{
-              width: '0.6lh',
-              height: '0.6lh',
-              fill: '#1b487d',
-
-              '@media (min-width: 60rem)': {
-                width: '0.8lh',
-                height: '0.8lh',
-              },
-            }}
-          >
-            <path d="M320-240h320v-80H320v80zm0-160h320v-80H320v80zM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240zm280-520v-200H240v640h480v-440H520zM240-800v200-200 640-640z" />
-          </svg>
-        </div>
         {children}
       </aside>
     )
