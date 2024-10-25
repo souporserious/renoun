f9c07f71015e861be98d539eced4fa8872faff8c
Travis Arnold <ftntravis@gmail.com>
2024-10-24 21:33:05 -0700

    fix focused lines

--- apps/site/app/(home)/QuickSteps.tsx
+++ apps/site/app/(home)/QuickSteps.tsx
@@ -37,7 +37,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return <Content />
 }`,
     codeBlockProps: {
-      focusedLines: '8-15',
+      focusedLines: '8-18',
     },
   },
   {
