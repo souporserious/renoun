5d15d2c0684d812cb520069d26e57349ee82a724
Travis Arnold <ftntravis@gmail.com>
2024-10-26 16:03:48 -0700

    add test for tree navigation

--- packages/renoun/src/collections/index.test.ts
+++ packages/renoun/src/collections/index.test.ts
@@ -1,10 +1,43 @@
 import type { ComponentType } from 'react'
-import { describe, it, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 
-import { Collection, CompositeCollection } from './index'
+import {
+  Collection,
+  CompositeCollection,
+  isFileSystemSource,
+  isExportSource,
+  type FileSystemSource,
+  type ExportSource,
+} from './index'
+
+function filterInternalSources(
+  source: ExportSource<any> | FileSystemSource<any>
+) {
+  if (isFileSystemSource(source)) {
+    if (source.isFile()) {
+      const allInternal = source
+        .getExports()
+        .every((exportSource) =>
+          exportSource.getTags()?.every((tag) => tag.tagName === 'internal')
+        )
+
+      if (allInternal) {
+        return false
+      }
+    }
+  }
+
+  if (isExportSource(source)) {
+    if (source.getTags()?.find((tag) => tag.tagName === 'internal')) {
+      return false
+    }
+  }
+
+  return true
+}
 
 describe('collections', () => {
-  it('merges composite collection siblings', async () => {
+  test('merges composite collection siblings', async () => {
     const CollectionsCollection = new Collection({
       filePattern: 'src/collections/index.tsx',
       baseDirectory: 'collections',
@@ -23,5 +56,55 @@ describe('collections', () => {
     const [, nextSource] = await source.getSiblings()
 
     expect(nextSource).toBeDefined()
+  })
+
+  test('generating tree navigation', async () => {
+    const ComponentsCollection = new Collection<{
+      [key: string]: ComponentType
+    }>({
+      filePattern: 'src/components/**/*.{ts,tsx}',
+      baseDirectory: 'components',
+      filter: filterInternalSources,
+    })
+
+    async function buildTreeNavigation(source: FileSystemSource<any>) {
+      const sources = await source.getSources({ depth: 1 })
+      const path = source.getPath()
+
+      if (sources.length === 0) {
+        return path
+      }
+
+      return {
+        path,
+        children: await Promise.all(sources.map(buildTreeNavigation)),
+      }
+    }
+
+    const sources = await ComponentsCollection.getSources({ depth: 1 })
+    const tree = await Promise.all(sources.map(buildTreeNavigation))
+
+    expect(tree).toMatchInlineSnapshot(`
+      [
+        "/components",
+        "/api-reference",
+        {
+          "children": [
+            "/code-block/copy-button",
+            "/code-block/line-numbers",
+            "/code-block/tokens",
+            "/code-block/toolbar",
+          ],
+          "path": "/code-block",
+        },
+        "/code-inline",
+        "/copyright",
+        "/git-provider",
+        "/mdx-components",
+        "/mdx-content",
+        "/package-install",
+        "/rendered-html",
+      ]
+    `)
   })
 })
