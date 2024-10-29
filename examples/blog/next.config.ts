5fc5874cb2d10df8fbde1ee3cd03a3fdea035fb2
Travis Arnold <ftntravis@gmail.com>
2024-10-29 16:58:16 -0700

    remove unnecessary webpack config

--- examples/blog/next.config.ts
+++ examples/blog/next.config.ts
@@ -1,7 +1,6 @@
 import createMDXPlugin from '@next/mdx'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import webpack from 'webpack'
 
 const withMDX = createMDXPlugin({
   extension: /\.mdx?$/,
@@ -13,18 +12,4 @@ const withMDX = createMDXPlugin({
 export default withMDX({
   output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  webpack(config) {
-    config.plugins.push(
-      new webpack.ContextReplacementPlugin(
-        /\/(@ts-morph\/common)\//,
-        (data) => {
-          for (const dependency of data.dependencies) {
-            delete dependency.critical
-          }
-          return data
-        }
-      )
-    )
-    return config
-  },
 })
