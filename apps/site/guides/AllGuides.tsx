36b43cb887ce727b288a2c1a7cc26c18055aef12
Travis Arnold <ftntravis@gmail.com>
2024-11-30 10:26:47 -0800

    source -> entry

--- apps/site/guides/AllGuides.tsx
+++ apps/site/guides/AllGuides.tsx
@@ -4,13 +4,13 @@ import { Card } from '@/components/Card'
 export async function AllGuides() {
   const entries = await GuidesCollection.getEntries()
 
-  return entries.map(async (source, index) => {
-    const metadata = await source.getExport('metadata').getRuntimeValue()
+  return entries.map(async (entry, index) => {
+    const metadata = await entry.getExport('metadata').getRuntimeValue()
 
     return (
       <Card
         key={index}
-        href={source.getPath()}
+        href={entry.getPath()}
         label={metadata.label ?? metadata.title}
       />
     )
