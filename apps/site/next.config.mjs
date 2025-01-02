4f2364b93e0819efad2b823a452bf182150989ea
Travis Arnold <ftntravis@gmail.com>
2025-01-02 12:46:46 -0800

    remove webpack configuration

--- apps/site/next.config.mjs
+++ apps/site/next.config.mjs
@@ -1,6 +1,5 @@
 import createMDXPlugin from '@next/mdx'
 import { remarkPlugins, rehypePlugins } from 'renoun/mdx'
-import webpack from 'webpack'
 
 const withMDX = createMDXPlugin({
   extension: /\.mdx?$/,
@@ -15,22 +14,4 @@ export default withMDX({
   output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   transpilePackages: ['renoun'],
-  webpack(config) {
-    config.resolve.extensionAlias = {
-      '.js': ['.ts', '.tsx', '.js'],
-    }
-
-    config.plugins.push(
-      new webpack.ContextReplacementPlugin(
-        /\/(@ts-morph\/common)\//,
-        (data) => {
-          for (const dependency of data.dependencies) {
-            delete dependency.critical
-          }
-          return data
-        }
-      )
-    )
-    return config
-  },
 })
