440f9a5d3db2a1540c097c0b222edf6d5fcc4904
Travis Arnold <ftntravis@gmail.com>
2025-01-01 22:01:27 -0800

    add example links

--- apps/site/components/Row.tsx
+++ apps/site/components/Row.tsx
@@ -1,9 +1,22 @@
-export function Row({ children }: { children: React.ReactNode }) {
+const variants = {
+  large: '16rem',
+  medium: '12rem',
+}
+
+export function Row({
+  children,
+  variant = 'medium',
+}: {
+  children: React.ReactNode
+  variant?: keyof typeof variants
+}) {
+  const width = variants[variant]
+
   return (
     <div
       css={{
         display: 'grid',
-        gridTemplateColumns: `repeat(auto-fill, minmax(12rem, 1fr))`,
+        gridTemplateColumns: `repeat(auto-fill, minmax(${width}, 1fr))`,
         gap: '1rem',
       }}
     >
---
+++ apps/site/docs/Examples.tsx
@@ -0,0 +1,48 @@
+import { styled } from 'restyle'
+import { Row } from '@/components/Row'
+
+const StyledLink = styled('a', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRadius: '0.25rem',
+  backgroundColor: 'var(--color-surface-interactive)',
+  ':hover': {
+    backgroundColor: 'var(--color-surface-interactive-highlighted)',
+    textDecoration: 'none !important',
+  },
+  '& h3': {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    aspectRatio: '21 / 9',
+    margin: 0,
+    backgroundColor: 'var(--color-surface-secondary)',
+    color: 'var(--color-foreground-secondary)',
+  },
+  '& p': {
+    textAlign: 'center',
+    padding: '1.5rem 0.75rem',
+    letterSpacing: '0.025em',
+    textWrap: 'balance',
+    color: 'var(--color-foreground-interactive)',
+  },
+})
+
+export function Examples() {
+  return (
+    <Row variant="large">
+      <StyledLink href="https://github.com/souporserious/renoun/tree/main/examples/blog">
+        <h3>Blog</h3>
+        <p>A list of posts and a detail view for each post.</p>
+      </StyledLink>
+      <StyledLink href="https://github.com/souporserious/renoun/tree/main/examples/design-system">
+        <h3>Design System</h3>
+        <p>Automated component documentation with examples.</p>
+      </StyledLink>
+    </Row>
+  )
+}
