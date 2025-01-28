1b73d72a6c7c3873c6218ff296cef6ef3afd8826
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2025-01-28 15:59:51 +0100

    ugly, but it works

--- packages/git-file-system/src/index.test.ts
+++ packages/git-file-system/src/index.test.ts
@@ -1,9 +1,9 @@
 import { describe, test, expect } from 'vitest'
 
-import { createCheckoutConfig, createCloneConfig } from './index'
+import { createCheckoutConfig, createCloneConfig, loadContent } from './index'
 import path from 'path'
 
-describe('git filesystem', () => {
+describe('git filesystem - config', () => {
   describe('clone config', () => {
     test('w/ defaults', () => {
       const config = createCloneConfig({
@@ -58,9 +58,18 @@ describe('git filesystem', () => {
         password: 'XXXX',
       })
     })
+
+    test('w/ branch', () => {
+      const config = createCloneConfig({
+        repository: 'https://github.com/souporserious/renoun',
+        branch: 'custom-branch',
+      })
+
+      expect(config.ref).toEqual('custom-branch')
+    })
   })
 
-  describe("checkout config", () => {
+  describe('checkout config', () => {
     test('w/ defaults', () => {
       const config = createCheckoutConfig({
         repository: 'https://github.com/souporserious/renoun',
@@ -85,5 +94,38 @@ describe('git filesystem', () => {
 
       expect(config.dir).toEqual('/tmp')
     })
+
+    test('w/ custom include', () => {
+      const config = createCheckoutConfig({
+        repository: 'https://github.com/souporserious/renoun',
+        include: ['content'],
+      })
+
+      expect(config.filepaths).toEqual(['content'])
+    })
+
+    test('w/ custom branch', () => {
+      const config = createCheckoutConfig({
+        repository: 'https://github.com/souporserious/renoun',
+        branch: 'custom-branch',
+      })
+
+      expect(config.ref).toEqual('custom-branch')
+    })
+  })
+})
+
+// TODO: currently we're generating files in the local fs
+//       maybe we should mock it? https://vitest.dev/guide/mocking#file-system
+describe('git filesystem - loader', () => {
+  test('loadContent', async () => {
+    const result = await loadContent({
+      repository: 'https://github.com/noxify/renoun-docs-template',
+      include: ['content/posts'],
+    })
+
+    expect([...result.getFiles().keys()]).toEqual([
+      './content/posts/build-a-button-component-in-react.mdx',
+    ])
   })
 })
--- packages/git-file-system/src/index.ts
+++ packages/git-file-system/src/index.ts
@@ -1,8 +1,17 @@
 import { createHash } from 'node:crypto'
 
-import { checkout, clone } from 'isomorphic-git'
+import { MemoryFileSystem } from 'renoun/file-system'
+
+import git, {
+  checkout,
+  clone,
+  listFiles,
+  statusMatrix,
+  type CallbackFsClient,
+  type PromiseFsClient,
+} from 'isomorphic-git'
 import path from 'path'
-import fs from 'fs/promises'
+import defaultFs from 'fs/promises'
 import http from 'isomorphic-git/http/node'
 
 export type LoadContentProps = {
@@ -14,14 +23,57 @@ export type LoadContentProps = {
   }
   cacheDirectory?: string
   proxy?: string
+  include?: string[]
 }
 
-export async function loadContent(props: LoadContentProps) {
+// TODO: Check if we can use the methods from the `MemoryFileSystem`
+//       directly while running the isogit functions ( e.g. `clone` or `checkout` )
+function getFilesystemWrapper(memoryFs: MemoryFileSystem) {
+  return {
+    readFile: () => memoryFs.readFileSync,
+    writeFile: memoryFs.createFile,
+    mkdir: () => null,
+    rmdir: () => null,
+    unlink: () => null,
+    stat: () => null,
+    lstat: () => null,
+    readdir: () => memoryFs.readDirectorySync,
+    readlink: () => null,
+    symlink: () => null,
+    
+  }
+}
+
+export async function loadContent(props: LoadContentProps): Promise<MemoryFileSystem> {
+  const memoryFs = new MemoryFileSystem({})
+  // const fs = getFilesystemWrapper(memoryFs)
+  let cache = {}
+
+
+  const cacheDirectory = getCacheDirectory(props)
+  
+  // we can also 
   const cloneConfig = createCloneConfig(props)
+  const checkoutConfig = createCheckoutConfig(props)
 
-  console.dir(cloneConfig, { depth: null })
+  // TODO: try to integrate the `MemoryFileSystem` 
+  //       together with `getFileSystemWrapper`
+  await clone({ ...cloneConfig, cache })
+  await checkout({ ...checkoutConfig, cache })
+
+  const files = await listFiles({fs: defaultFs, dir: cacheDirectory, ref: props.branch, cache})
+
+  // TODO: Maybe we should add an check for file extensions here, 
+  //       otherwise we would also load images and other unsupported file types
+  for(const filePath of files) {
+    const fileContent = await defaultFs.readFile(path.join(cacheDirectory, filePath), 'utf8')
+    memoryFs.createFile(filePath, fileContent)
+  }
+
+  cache = {}
+
+  return memoryFs
 }
-
 
 // calculates the path to store the remote files locally
 // if `cacheDirectory` is specified, we will use this path
@@ -33,22 +85,33 @@ function getCacheDirectory(props: LoadContentProps) {
   if (props.cacheDirectory) {
     return props.cacheDirectory
   }
-  const hash = createHash('sha256');
-  hash.update(`${props.repository}-${props.branch ?? "main"}`)
+  const hash = createHash('sha256')
+  hash.update(`${props.repository}-${props.branch ?? 'main'}`)
 
-  return path.join(process.cwd(), '.renoun', 'cache', 'git-file-system', hash.digest("hex"))
+  return path.join(
+    process.cwd(),
+    '.renoun',
+    'cache',
+    'git-file-system',
+    hash.digest('hex')
+  )
 }
 
 // Creates the configuration to run the `clone` command
 // Doc: https://isomorphic-git.org/docs/en/clone
-export function createCloneConfig(props: LoadContentProps) {
+export function createCloneConfig(
+  props: LoadContentProps,
+  fs?: CallbackFsClient | PromiseFsClient
+) {
   return {
     singleBranch: true,
     depth: 1,
     url: props.repository,
     corsProxy: props.proxy,
     dir: getCacheDirectory(props),
-    fs,
+    noCheckout: true,
+    ref: props.branch,
+    fs: fs ?? defaultFs,
     http,
     onAuth: props.credentials
       ? () => {
@@ -63,10 +126,15 @@ export function createCloneConfig(props: LoadContentProps) {
 
 // Creates the configuration to run the `checkout` command
 // Doc: https://isomorphic-git.org/docs/en/checkout
-export function createCheckoutConfig(props: LoadContentProps) {
+export function createCheckoutConfig(
+  props: LoadContentProps,
+  fs?: CallbackFsClient | PromiseFsClient
+) {
   return {
     dir: getCacheDirectory(props),
-    fs,
+    fs: fs ?? defaultFs,
+    filepaths: props.include,
+    ref: props.branch,
     force: true, // always override local changes
   } as Parameters<typeof checkout>[number]
 }
