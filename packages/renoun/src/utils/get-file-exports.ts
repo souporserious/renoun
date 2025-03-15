c35d7abdc7e8ae598f3681f40c2004c0300161ca
Travis Arnold <ftntravis@gmail.com>
2025-03-15 13:49:28 -0700

    consolidate type

--- packages/renoun/src/utils/get-file-exports.ts
+++ packages/renoun/src/utils/get-file-exports.ts
@@ -15,12 +15,7 @@ export interface FileExport {
 export function getFileExports(
   filePath: string,
   project: Project
-): {
-  name: string
-  path: string
-  position: number
-  kind: tsMorph.SyntaxKind
-}[] {
+): FileExport[] {
   let sourceFile = project.getSourceFile(filePath)
 
   if (!sourceFile) {
