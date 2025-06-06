ce8178cfcad90e6476bbe7705bf4e16f9307cbaa
Travis Arnold <ftntravis@gmail.com>
2025-06-06 12:43:16 -0700

    tighten up card styles

--- examples/docs/ui/Card.tsx
+++ examples/docs/ui/Card.tsx
@@ -9,17 +9,19 @@ export function Card({
   title: string
   children: React.ReactNode
 }) {
+  const ContentElement = typeof children === 'string' ? 'p' : 'div'
+
   return (
-    <div className="flex flex-col rounded-2xl p-6 bg-gray-50 dark:bg-gray-800 h-full">
-      <div className="flex items-center justify-center w-12 h-12 mb-4 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-white rounded-full p-2 text-xl">
+    <div className="flex flex-col gap-6 p-6 bg-gray-50 dark:bg-gray-800 h-full rounded-2xl">
+      <div className="flex items-center justify-center w-12 h-12 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-white rounded-full p-2 text-xl">
         {icon}
       </div>
-      <div className="font-semibold text-lg mb-1 text-gray-900 dark:text-gray-100">
+      <div className="font-semibold text-lg text-gray-900 dark:text-gray-100">
         {title}
       </div>
-      <div className="text-gray-600 dark:text-gray-300 text-base">
+      <ContentElement className="text-gray-600 dark:text-gray-300 text-base m-0!">
         {children}
-      </div>
+      </ContentElement>
     </div>
   )
 }
