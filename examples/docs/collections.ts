739dae9ef3e3056bdf071ea845676e00223cf9c9
Travis Arnold <ftntravis@gmail.com>
2025-06-24 15:53:03 -0700

    fix routes

--- examples/docs/collections.ts
+++ examples/docs/collections.ts
@@ -1,4 +1,4 @@
-import { Directory, withSchema } from 'renoun/file-system'
+import { Directory, resolveFileFromEntry, withSchema } from 'renoun/file-system'
 import { z } from 'zod'
 
 export const docs = new Directory({
@@ -23,16 +23,20 @@ export const docs = new Directory({
 
 export const routes = docs.getEntries({ recursive: true }).then((entries) =>
   Promise.all(
-    entries.map(async (doc) => ({
-      pathname: doc.getPathname(),
-      segments: doc.getPathnameSegments({ includeBasePathname: false }),
-      title: await doc
-        .getExportValue('metadata')
-        .then((metadata) => metadata.title),
-      depth: doc.getDepth(),
-      order: await doc
-        .getExportValue('metadata')
-        .then((metadata) => metadata.order),
-    }))
+    entries.map(async (doc) => {
+      const file = await resolveFileFromEntry(doc)
+      const metadata = file ? await file.getExportValue('metadata') : undefined
+
+      if (!metadata) {
+        throw new Error(`Metadata export not defined for ${doc.getPathname()}`)
+      }
+
+      return {
+        pathname: doc.getPathname(),
+        segments: doc.getPathnameSegments({ includeBasePathname: false }),
+        title: metadata.title,
+        order: metadata.order,
+      }
+    })
   )
 )
