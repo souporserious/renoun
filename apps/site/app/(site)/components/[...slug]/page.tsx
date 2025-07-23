3cb5b69c61eb1ef9d9bdc8f9535c0785a6e19c95
Travis Arnold <ftntravis@gmail.com>
2025-07-23 10:52:06 -0700

    fix preview width

--- apps/site/app/(site)/components/[...slug]/page.tsx
+++ apps/site/app/(site)/components/[...slug]/page.tsx
@@ -306,7 +306,6 @@ async function Preview({
             css={{
               fontSize: '1rem',
               lineHeight: '1.35',
-              width: '100%',
               maxWidth: '-webkit-fill-available',
               padding: '4rem',
               margin: 'auto',
