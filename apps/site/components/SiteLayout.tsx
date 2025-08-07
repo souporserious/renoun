0f854fdd67d764e5ea5ffba5091242b9947feb55
Travis Arnold <ftntravis@gmail.com>
2025-08-07 12:59:24 -0700

    tighten header spacing

--- apps/site/components/SiteLayout.tsx
+++ apps/site/components/SiteLayout.tsx
@@ -177,7 +177,7 @@ export function SiteLayout({
             gridTemplateColumns: 'subgrid',
             gridColumn: sidebar ? '4 / -2' : '2 / -2',
             gridRow: '2',
-            padding: '6.25rem 0',
+            padding: '4rem 0',
           },
         }}
       >
