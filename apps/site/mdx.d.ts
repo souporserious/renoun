fd09899350c17dc354280f8efc87604b801ccfcd
Travis Arnold <ftntravis@gmail.com>
2025-08-26 15:23:52 -0700

    use MDXHeadings type for augmentation

--- apps/site/mdx.d.ts
+++ apps/site/mdx.d.ts
@@ -1,8 +1,3 @@
 declare module '*.mdx' {
-  export const headings: {
-    id: any
-    level: number
-    children: React.ReactNode
-    text: string
-  }[]
+  export const headings: import('renoun/mdx').MDXHeadings
 }
