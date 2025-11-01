3b558c5be0c303a8eefd8d5703fc0f0334ebb5f2
Travis Arnold <ftntravis@gmail.com>
2025-10-31 21:20:28 -0700

    fix dropdown link styles

--- apps/site/components/NavigationLink.tsx
+++ apps/site/components/NavigationLink.tsx
@@ -2,19 +2,14 @@
 import type { ComponentProps } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { styled, type CSSObject } from 'restyle'
-
-const StyledLink = styled(Link, {
-  fontSize: 'var(--font-size-body-2)',
-  padding: '0.25rem 0.5rem',
-  whiteSpace: 'nowrap',
-})
+import { css, type CSSObject } from 'restyle'
 
 export function NavigationLink({
   href,
   activeColor = 'var(--color-foreground)',
   activePathnames,
-  css,
+  css: cssProp,
+  className,
   children,
   ...props
 }: {
@@ -31,22 +26,32 @@ export function NavigationLink({
   const styles: CSSObject = {
     display: 'block',
     padding: '0.5rem 0.75rem',
+    fontSize: 'var(--font-size-body-2)',
+    whiteSpace: 'nowrap',
+    ...cssProp,
   }
 
   if (isActive) {
     styles.color = activeColor
     styles.textShadow = '0.01em 0 currentColor,-0.01em 0 currentColor'
   } else {
-    styles.color = css?.color ?? 'var(--color-foreground-interactive)'
+    styles.color = cssProp?.color || 'var(--color-foreground-interactive)'
     styles[':hover'] = {
       color: 'var(--color-foreground-interactive-highlighted)',
-      ...(css?.[':hover'] as CSSObject),
+      ...(cssProp?.[':hover'] as CSSObject),
     }
   }
 
+  const [classNames, Styles] = css(styles)
+
   return (
-    <StyledLink href={href} css={{ ...styles, ...css }} {...props}>
+    <Link
+      href={href}
+      className={className ? `${classNames} ${className}` : classNames}
+      {...props}
+    >
       {children}
-    </StyledLink>
+      <Styles />
+    </Link>
   )
 }
